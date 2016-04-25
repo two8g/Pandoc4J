a5a4f3c5157c6222a83f613b774ef0233ce00065
two8g <ybxnice@gmail.com>
2016-04-25 17:28:01 +0800

    add extension

---
+++ src/main/java/galvin/pandoc/Extension.java
@@ -0,0 +1,29 @@
+package galvin.pandoc;
+
+/**
+ * list Non-pandoc extensions.
+ * http://pandoc.org/README.html#non-pandoc-extensions
+ * Created by two8g on 16-4-25.
+ */
+public enum Extension {
+
+	tex_math_dollars("tex_math_dollars", Format.html),
+	tex_math_single_backslash("tex_math_single_backslash", Format.html),
+	tex_math_double_backslash("tex_math_double_backslash", Format.html);
+
+	private String extension;
+	private Format format;
+
+	private Extension(String extension, Format format) {
+		this.extension = extension;
+		this.format = format;
+	}
+
+	public String extension() {
+		return extension;
+	}
+
+	public Format format() {
+		return format;
+	}
+}
